88916bda54b9aec9feea5dd4035601bbcb9096ac
gutovesco <guto.vesco@hotmail.com>
2020-10-13 14:52:15 -0300

    test with firebase

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -1,10 +1,12 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:http/http.dart' as http;
 
 class Users with ChangeNotifier {
+  static const _baseUrl = 'https://simple-crud-d8314.firebaseio.com/';
   Map<String, User> _items = {...MOCK};
 
   List<User> get all {
@@ -19,7 +21,7 @@ class Users with ChangeNotifier {
     return _items.values.elementAt(i);
   }
 
-  void put(User user) {
+  Future<void> put(User user) async {
     if (user == null) {
       return;
     }
@@ -27,24 +29,49 @@ class Users with ChangeNotifier {
     if (user.id != null &&
         user.id.trim().isNotEmpty &&
         _items.containsKey(user.id)) {
+      await http.post(
+        "$_baseUrl/users.json",
+        body: json.encode(
+          {
+            'email': user.email,
+            'name': user.name,
+            'avatarUrl': user.avatarUrl,
+          },
+        ),
+      );
+
       _items.update(
-          user.id,
-          (_) => User(
-                email: user.email,
-                name: user.name,
-                id: user.id,
-                avatarUrl: user.avatarUrl,
-              ));
+        user.id,
+        (_) => User(
+          email: user.email,
+          name: user.name,
+          id: user.id,
+          avatarUrl: user.avatarUrl,
+        ),
+      );
     } else {
-      final id = Random().nextDouble().toString();
+      final response = await http.post(
+        "$_baseUrl/users.json",
+        body: json.encode(
+          {
+            'email': user.email,
+            'name': user.name,
+            'avatarUrl': user.avatarUrl,
+          },
+        ),
+      );
+
+      final id = json.decode(response.body)['name'];
 
       _items.putIfAbsent(
-          id,
-          () => User(
-              email: user.email,
-              name: user.name,
-              avatarUrl: user.avatarUrl,
-              id: id));
+        id,
+        () => User(
+          email: user.email,
+          name: user.name,
+          avatarUrl: user.avatarUrl,
+          id: id,
+        ),
+      );
     }
 
     notifyListeners();
